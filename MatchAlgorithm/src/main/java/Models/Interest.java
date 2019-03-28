bf9ac440310af13d4a710435d8fe6f23e28318c0
Olivier Bouchoms <olivier_2000@live.nl>
2019-03-29 00:42:18 +0100

    Implement equals() and hashcode() in Interest

--- MatchAlgorithm/src/main/java/Models/Interest.java
+++ MatchAlgorithm/src/main/java/Models/Interest.java
@@ -1,5 +1,7 @@
 package Models;
 
+import java.util.Objects;
+
 public class Interest {
 
     private int id;
@@ -28,5 +30,18 @@ public class Interest {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Interest interest = (Interest) o;
+        return Objects.equals(name, interest.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
